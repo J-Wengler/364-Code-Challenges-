a4a13aad923c53802444c53c38f23a0761caf6d2
Drew Capener <amclanecapener@gmail.com>
2021-12-01 00:38:34 -0700

    Finished bw matching

--- bw-matching/main.py
+++ bw-matching/main.py
@@ -1,40 +1,40 @@
-from typing import Pattern
+def get_index_for_appearance(column, appearance, symbol):
+    num_appearances = 0
+    for i in range(len(column)):
+        if column[i] == symbol:
+            num_appearances += 1
+        if num_appearances == appearance:
+            return i
 
-def get_symbol(lst):
-    return lst[0]
+def get_num_appearances(column, i):
+    num_appearances = 0
+    search_symbol = column[i]
+    for j in range(len(column)):
+        if column[j] == search_symbol:
+            num_appearances += 1
+        if j == i:
+            break
+    return num_appearances
+
 
 def build_last_to_first(last_column):
-    # this function should be used to build a
-    # dict last_to_first where the keys are 
-    # indexes in the last_column mapped to 
-    # values that are the corresponding indexes
-    # in the first_column
     last_to_first = {}
-    symbol_counts = {}
-    modified_last_column = []
-    i = 0
-    for symbol in last_column:
-        if symbol not in symbol_counts.keys():
-            symbol_counts[symbol] = 1
-        else:
-            symbol_counts[symbol] += 1
-        modified_last_column.append([symbol, symbol_counts[symbol], i])
-        i += 1
-    modified_first_column = sorted(modified_last_column, key=get_symbol)
-    for i in range(len(modified_first_column)):
-        last_to_first[modified_first_column[i][2]] = i
+    first_column = ''.join(sorted(last_column))
+    for i in range(len(last_column)):
+        last_to_first[i] = get_index_for_appearance(first_column, get_num_appearances(last_column, i), last_column[i])
     return last_to_first
+
 
 def bw_matching(last_column, pattern, last_to_first):
     top = 0
     bottom = len(last_column) - 1
     while top <= bottom:
-        if not pattern:
+        if pattern:
             symbol = pattern[-1]
             pattern = pattern[:-1]
-            if symbol in last_column:
-                top_index = last_column.index(symbol)
-                bottom_index = len(last_column) - 1 - last_column[::-1].index(symbol)
+            if symbol in last_column[top: bottom + 1]:
+                top_index = last_column.index(symbol, top, bottom + 1)
+                bottom_index = len(last_column) - 1 - last_column[::-1].index(symbol, len(last_column) - bottom - 1, len(last_column) - top + 1)
                 top = last_to_first[top_index]
                 bottom = last_to_first[bottom_index]
             else:
@@ -49,7 +49,6 @@ def main():
         last_column = in_file.readline().strip()
         patterns = in_file.readline().split()
 
-    # I do not think this is working LOL
     last_to_first = build_last_to_first(last_column)
 
     for pattern in patterns:
